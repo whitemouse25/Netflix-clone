ea45cf53f0202cfd590d4d54c775b057af3b88c8
Sandesh Gorathoki <whimosandesh@gmail.com>
2025-02-04 12:12:37 -0500

    Remove the unnecssary things in the frontend page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
       </div>
       <div className="navbar-right">
         <img src={search_icon} alt="" className="icons" />
-        <p>Children</p>
+
         <img src={bell_icon} alt="" className="icons" />
         <div className="navbar-profile">
           <img src={profile_img} alt="" className="profile" />
